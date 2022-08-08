931cda52117687e79d57f72b9f9155fc9ecaa4ad
Dhyey Mavani <ddmavani2003@gmail.com>
2022-08-07 21:24:55 -0400

    completed prework

---
+++ app/src/main/java/com/dhyeymavani/helloworld_dhyey/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dhyeymavani.helloworld_dhyey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.textView).setTextColor(resources.getColor(R.color.white))
+        }
+    }
+}
